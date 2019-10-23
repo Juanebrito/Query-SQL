aed87787ab10fdaee7ef307a940c27c039a07962
Juan Brito <juanebrito3.1@gmail.com>
2019-10-22 23:40:46 -0400

    vercion 2

---
+++ carrera.sql
@@ -0,0 +1,7 @@
+INSERT INTO carrera(nombre)  VALUES ('Aeronautica');
+
+INSERT INTO carrera(nombre)  VALUES ('Administración');
+
+UPDATE carrera  set nombre='aviacion' where id='14'and nombre='Aeronautica';
+
+DElETE from carrera where nombre= 'Emfermeria'and id='11';
---
+++ estudiante.sql
@@ -0,0 +1,5 @@
+INSERT INTO estudiante(nombre,matricula, edad)  VALUES ('Mia Isabela', '1909','21');
+
+UPDATE estudiante  set matricula='AA2012' where id='10'and matricula='AA2020';
+
+DElETE from estudiante where nombre= 'lola' and matricul= 'bb987'and id='8';
